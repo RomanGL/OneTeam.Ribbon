145a61ea34e01f602feebf04037ad611ac6a6c63
Roman Gladkikh <roman.gladkikh@outlook.com>
2017-01-30 01:44:59 +0700

    Начальный комит.

---
+++ OneTeam.Ribbon/Ribbon.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+
+namespace OneTeam.Ribbon
+{
+    public class Ribbon : ItemsControl
+    {
+        public Ribbon()
+        {
+            this.DefaultStyleKey = typeof(Ribbon);
+        }
+        
+        public int SelectedIndex
+        {
+            get { return (int)GetValue(SelectedIndexProperty); }
+            set { SetValue(SelectedIndexProperty, value); }
+        }
+        
+        public static readonly DependencyProperty SelectedIndexProperty =
+            DependencyProperty.Register("SelectedIndex", typeof(int), typeof(Ribbon), new PropertyMetadata(-1, OnSelectedIndexChanged));
+        
+        public object SelectedItem
+        {
+            get { return (object)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+        
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(object), typeof(Ribbon), new PropertyMetadata(null));
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _headersListView = GetTemplateChild("HeadersListView") as ListView;
+
+            _headersListView.ItemsSource = Items;
+            _headersListView.ItemClick += HeadersListView_ItemClick;
+        }
+
+        private void HeadersListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            SelectedIndex = Items.IndexOf(e.ClickedItem);
+        }
+
+        private static void OnSelectedIndexChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            int newIndex = (int) e.NewValue;
+            if (newIndex < 0)
+                return;
+
+            var ribbon = (Ribbon)obj;
+            ribbon.SelectedItem = ribbon.Items[newIndex];
+        }
+
+        private ListView _headersListView;
+    }
+}
---
+++ OneTeam.Ribbon/RibbonTab.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace OneTeam.Ribbon
+{
+    public class RibbonTab : ContentControl
+    {
+        public RibbonTab()
+        {
+            this.DefaultStyleKey = typeof(RibbonTab);
+        }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(RibbonTab), new PropertyMetadata(null));
+    }
+}
